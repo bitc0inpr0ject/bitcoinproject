d66599dcc177f6d0030c7a13c69fea9623005fa2
quanghuy1258 <quanghuy1258@gmail.com>
2018-07-13 09:37:53 +0700

    New version of BitcoinUtils

--- testnet/src/main/java/BitcoinUtils.java
+++ testnet/src/main/java/BitcoinUtils.java
@@ -1,57 +1,59 @@
-import wf.bitcoin.javabitcoindrpcclient.BitcoinJSONRPCClient;
-import wf.bitcoin.javabitcoindrpcclient.BitcoindRpcClient;
+import com.msgilligan.bitcoinj.rpc.BitcoinClient;
+import org.bitcoinj.core.NetworkParameters;
+import org.bitcoinj.core.Transaction;
+import org.bitcoinj.params.MainNetParams;
+import org.bitcoinj.params.TestNet2Params;
 
-import java.net.URL;
+import java.net.URI;
 import java.util.List;
 import java.util.concurrent.*;
 
 public class BitcoinUtils {
 
-    private static BitcoindRpcClient client = null;
+    private static BitcoinClient bitcoinClient = null;
 
-    public static BitcoindRpcClient getBitcoindRpcClientInstance() {
-        if (client == null) {
-            String server = "192.168.1.115:18332";
+    public static BitcoinClient getBitcoinClientInstance() {
+        if (bitcoinClient == null) {
+            String server = "http://192.168.1.115:18332";
             String username = "ndhy";
             String password = "12345";
+            int dev = 1; // dev = 1 --> testnet
             try {
-                URL url = new URL("http://" + username + ":" + password + "@" + server);
-                client = new BitcoinJSONRPCClient(url);
+                NetworkParameters network;
+                URI uri;
+                if (dev == 0) {
+                    network = new MainNetParams();
+                    uri = new URI(server);
+
+                } else {
+                    network = new TestNet2Params();
+                    uri = new URI(server);
+                }
+                System.out.println("bitcoinclient networkID" + network.getId());
+                System.out.println("uri server bitcoin: " + uri.toString());
+                bitcoinClient = new BitcoinClient(network, uri, username, password);
+                return bitcoinClient;
             } catch (Exception e) {
+                System.out.println("Can not connect to server and start bitcoinClient");
                 e.printStackTrace();
-                client = null;
+                bitcoinClient = null;
             }
         }
-        return client;
+        return bitcoinClient;
     }
 
-    public static List<String> getTransactionInBlock(int currentBlock) throws ExecutionException {
+    public static List<Transaction>  getTransactionInBlock(int currentBlock) throws ExecutionException {
         while (true) {
             ExecutorService executor = Executors.newCachedThreadPool();
-            Callable<List<String>> task = () -> client.getBlock(currentBlock).tx();
-            Future<List<String>> future = executor.submit(task);
+            Callable<List<Transaction>> task = () -> bitcoinClient.getBlock(currentBlock).getTransactions();
+            Future<List<Transaction>> future = executor.submit(task);
             try {
                 return future.get(5, TimeUnit.SECONDS);
             } catch (TimeoutException | InterruptedException ex) {
-                // no log --> do nothing
+                System.out.println("can not list transaction in block, retrying...");
+                ex.printStackTrace();
             } catch (ExecutionException e) {
-                throw e;
-            } finally {
-                future.cancel(true);
-            }
-        }
-    }
-
-    public static BitcoindRpcClient.RawTransaction getTransaction(String hash) throws ExecutionException {
-        while (true) {
-            ExecutorService executor = Executors.newCachedThreadPool();
-            Callable<BitcoindRpcClient.RawTransaction> task = () -> client.getRawTransaction(hash);
-            Future<BitcoindRpcClient.RawTransaction> future = executor.submit(task);
-            try {
-                return future.get(5, TimeUnit.SECONDS);
-            } catch (TimeoutException | InterruptedException ex) {
-                // no log --> do nothing
-            } catch (ExecutionException e) {
+                e.printStackTrace();
                 throw e;
             } finally {
                 future.cancel(true);
@@ -62,13 +64,15 @@ public class BitcoinUtils {
     public static int getBlockCount() throws ExecutionException {
         while (true) {
             ExecutorService executor = Executors.newCachedThreadPool();
-            Callable<Integer> task = () -> client.getBlockCount();
+            Callable<Integer> task = () -> bitcoinClient.getBlockCount();
             Future<Integer> future = executor.submit(task);
             try {
                 return future.get(5, TimeUnit.SECONDS);
             } catch (TimeoutException | InterruptedException ex) {
-                // no log --> do nothing
+                System.out.println("can not get Block Count detail, retrying...");
+                ex.printStackTrace();
             } catch (ExecutionException e) {
+                e.printStackTrace();
                 throw e;
             } finally {
                 future.cancel(true);
